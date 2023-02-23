7afe35b834abc16a0ef8d5f04fd3b4c74f958b2e
carlos benavides <1992cab@gmail.com>
2023-02-23 14:16:40 -0500

    feat: added local persistence to token and logout

--- src/app/account/account.service.ts
+++ src/app/account/account.service.ts
@@ -1,18 +1,37 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { IUser } from './models/user.model';
+import { Router } from '@angular/router';
 import { IAccount } from './models/account.model';
+import { ICredential } from './models/credential.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AccountService {
   private resourceUrl = `${environment.API_URL}/account`;
-  constructor(private http: HttpClient) {}
+  token: string;
 
-  login(user: Partial<IUser>): Observable<IAccount> {
-    return this.http.post<IAccount>(`${this.resourceUrl}/login`, user);
+  constructor(private http: HttpClient, private router: Router) {
+    this.token = localStorage.getItem('token') || '';
+  }
+
+  login(credential: Partial<ICredential>): Observable<IAccount> {
+    return this.http
+      .post<IAccount>(`${this.resourceUrl}/login`, credential)
+      .pipe(
+        map(res => {
+          this.token = res.Token;
+          localStorage.setItem('token', res.Token);
+          return res;
+        })
+      );
+  }
+
+  logout() {
+    this.token = '';
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
   }
 }
