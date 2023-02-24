bf39f7cbeb49cf5f9f3d094f9ef9140669d10c53
carlos benavides <1992cab@gmail.com>
2023-02-24 16:02:32 -0500

    refactor: renamed type in subscribe model

--- src/app/entities/subscribers/subscriber.model.ts
+++ src/app/entities/subscribers/subscriber.model.ts
@@ -22,16 +22,16 @@ export interface ISubscriber {
   Id: number;
 }
 
-export type IEditSubscriber = Pick<
-  ISubscriber,
-  | 'Id'
-  | 'Name'
-  | 'Email'
-  | 'CountryCode'
-  | 'PhoneNumber'
-  | 'JobTitle'
-  | 'Area'
-  | 'Topics'
+export type PickSubscriber = Partial<
+  Pick<
+    ISubscriber,
+    | 'Id'
+    | 'Name'
+    | 'Email'
+    | 'CountryCode'
+    | 'PhoneNumber'
+    | 'JobTitle'
+    | 'Area'
+    | 'Topics'
+  >
 >;
-
-export type INewSubscriber = Omit<IEditSubscriber, 'id'>;
