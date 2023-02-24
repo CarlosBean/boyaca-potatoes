1051771886043f2d6423482011458bcd7b74259f
carlos benavides <1992cab@gmail.com>
2023-02-24 16:08:46 -0500

    WIP: added base logic to edit and create subscriber

---
+++ src/app/entities/subscribers/subscriber-update/subscriber-update.component.ts
@@ -0,0 +1,125 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule, Location } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { SubscriberService } from '../subscriber.service';
+import { ActivatedRoute } from '@angular/router';
+import { EMPTY, forkJoin } from 'rxjs';
+import { PickSubscriber, ISubscriber } from '../subscriber.model';
+import { CountryService } from '../../countries/country.service';
+import { ICountry } from '../../countries/country.model';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-subscriber-update',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSnackBarModule,
+  ],
+  templateUrl: './subscriber-update.component.html',
+})
+export class SubscriberUpdateComponent implements OnInit {
+  subscriber!: ISubscriber;
+
+  saveloading = false;
+  initLoading = false;
+
+  countries: ICountry[] = [];
+
+  form = this.fb.nonNullable.group({
+    Name: ['', [Validators.required]],
+    Email: [''],
+    CountryCode: [''],
+    PhoneNumber: [''],
+    JobTitle: [''],
+    Area: [''],
+    Topics: [''],
+  });
+
+  constructor(
+    private fb: FormBuilder,
+    private subsService: SubscriberService,
+    private countryService: CountryService,
+    private route: ActivatedRoute,
+    private location: Location,
+    private snackbar: MatSnackBar
+  ) {}
+
+  ngOnInit() {
+    const params = this.route.snapshot.params;
+    const subId = params['id'];
+
+    const requests$ = {
+      countries: this.countryService.getAllCountries({ count: 255 }),
+      subscriber: subId ? this.subsService.getSubscriber(subId) : EMPTY,
+    };
+
+    this.initLoading = true;
+
+    forkJoin(requests$).subscribe(res => {
+      this.initLoading = false;
+      this.countries = res.countries.Data;
+
+      if (res.subscriber) {
+        this.subscriber = res.subscriber;
+        this.patchForm(res.subscriber);
+      }
+    });
+  }
+
+  goBack() {
+    this.location.back();
+  }
+
+  patchForm(data: PickSubscriber) {
+    this.form.patchValue({
+      Name: data.Name,
+      Email: data.Email,
+      CountryCode: data.CountryCode,
+      PhoneNumber: data.PhoneNumber,
+      JobTitle: data.JobTitle,
+      Area: data.Area,
+      Topics: data.Topics?.join(),
+    });
+  }
+
+  buildPayload(): PickSubscriber {
+    const submitted = this.form.value;
+
+    return {
+      Id: this.subscriber?.Id ?? null,
+      Name: submitted.Name,
+      Email: submitted.Email,
+      CountryCode: submitted.CountryCode,
+      PhoneNumber: submitted.PhoneNumber,
+      JobTitle: submitted.JobTitle,
+      Area: submitted.Area,
+      Topics: submitted.Topics?.split(','),
+    };
+  }
+
+  submit() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    const payload = this.buildPayload();
+    const action = this.subscriber ? 'updateSubscriber' : 'createSubscriber';
+
+    this.saveloading = true;
+
+    this.subsService[action](payload).subscribe(() => {
+      this.saveloading = false;
+      this.snackbar.open('It has been saved successfully.', undefined, {
+        panelClass: 'success',
+      });
+    });
+  }
+}
