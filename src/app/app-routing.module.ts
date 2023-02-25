23b4e10a7d39c8a3e4be729a1591972cad0955b1
carlos benavides <1992cab@gmail.com>
2023-02-25 11:08:00 -0500

    perf: added lazy load on dashboard components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './account/login/login.component';
 import { AuthGuard } from './core/guards/auth.guard';
-import { SubscriberListComponent } from './entities/subscribers/subscriber-list/subscriber-list.component';
-import { SubscriberUpdateComponent } from './entities/subscribers/subscriber-update/subscriber-update.component';
-import { DashboardComponent } from './layout/dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
@@ -14,12 +11,27 @@ const routes: Routes = [
   },
   {
     path: 'dashboard',
-    component: DashboardComponent,
+    loadComponent: () =>
+      import('./layout/dashboard/dashboard.component').then(
+        m => m.DashboardComponent
+      ),
     canActivate: [AuthGuard],
     children: [
       { path: '', pathMatch: 'full', redirectTo: 'subscribers' },
-      { path: 'subscribers', component: SubscriberListComponent },
-      { path: 'subscribers/:id', component: SubscriberUpdateComponent },
+      {
+        path: 'subscribers',
+        loadComponent: () =>
+          import(
+            './entities/subscribers/subscriber-list/subscriber-list.component'
+          ).then(m => m.SubscriberListComponent),
+      },
+      {
+        path: 'subscribers/:id',
+        loadComponent: () =>
+          import(
+            './entities/subscribers/subscriber-update/subscriber-update.component'
+          ).then(m => m.SubscriberUpdateComponent),
+      },
     ],
   },
   { path: '**', component: LoginComponent },
