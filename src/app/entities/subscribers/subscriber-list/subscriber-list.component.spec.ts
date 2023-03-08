1ccec9e7a43d40df5d81f0e7d4bd774c2d3d6704
carlos benavides <1992cab@gmail.com>
2023-03-08 00:13:19 -0500

    test: added missing providers to create component

--- src/app/entities/subscribers/subscriber-list/subscriber-list.component.spec.ts
+++ src/app/entities/subscribers/subscriber-list/subscriber-list.component.spec.ts
@@ -1,4 +1,8 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslocoTestingModule } from '@ngneat/transloco';
+import es from '../../../../assets/i18n/es.json';
+import en from '../../../../assets/i18n/en.json';
 
 import { SubscriberListComponent } from './subscriber-list.component';
 
@@ -8,9 +12,22 @@ describe('SubscriberListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ SubscriberListComponent ]
-    })
-    .compileComponents();
+      imports: [
+        SubscriberListComponent,
+        HttpClientTestingModule,
+        TranslocoTestingModule.forRoot({
+          langs: {
+            en,
+            es,
+          },
+          translocoConfig: {
+            availableLangs: ['en', 'es'],
+            defaultLang: 'es',
+          },
+          preloadLangs: true,
+        }),
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SubscriberListComponent);
     component = fixture.componentInstance;
